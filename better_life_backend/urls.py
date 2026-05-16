75759b3e28f9b91febdc0b279211de8ca6ffb844
amelevila <amelevila78@gmail.com>
2026-05-16 22:29:46 +0200

    Corregeix urls.py i test-requirements per fer passar pytest i CI (SCRUM-54)

--- better_life_backend/urls.py
+++ better_life_backend/urls.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import include
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
-    # Envolvemos todo bajo el prefijo 'api/' para que Nginx conecte a la primera
-    path('api/', include([
-        # Panel de administración
-        path('admin/', admin.site.urls),
-        
-        # Rutas de la documentación (Swagger) integradas en el prefijo
-        path('schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-        
-        # El conector original de tus compañeros (¡así no rompemos nada!)
-        path('', include('api.urls')),
-    ])),
+    path("admin/", admin.site.urls),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("", include("api.urls")),
 ]
